942f58362a8199af8d83f4d6e92aa69fe2ff13bc
chiayewken <chiayewken@gmail.com>
2024-05-24 19:15:45 +0800

    Add intro text

--- app.py
+++ app.py
@@ -252,7 +252,7 @@ def show_results(folder: str):
 
 
 def show_about():
-    st.write("Placeholder: This is an LLM Arena Leaderboard from Alibaba")
+    st.write("This is an Auto Chatbot Arena from Alibaba DAMO Academy.")
 
 
 def show_links():
@@ -271,8 +271,10 @@ def main():
     language = st.selectbox("Evaluation Language", ["English", "Chinese"])
     folder = "data/main_tour_40" if language == "English" else "data/main_tour_40_zh"
     tabs = st.tabs(["Leaderboard Results", "Debate Samples", "About Us"])
+    intro = "We introduce the Auto Chatbot Arena, an automated and reliable framework for evaluating large language models in a human-like manner. We generate diverse and challenging questions across various domains, including writing, reasoning, and knowledge. During the debate battle stage, two models take turns answering questions and responding to each other's arguments, showcasing their deeper capabilities. Finally, a committee of the top-performing models is formed to review the debate process and judge the final winner through multi-round discussions. Below, we present our leaderboard of popular models:"
 
     with tabs[0]:
+        st.write(intro)
         show_results(folder)
     with tabs[1]:
         show_debates(folder)
